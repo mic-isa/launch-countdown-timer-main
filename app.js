cb2d5dd3488164a1d313c6aabcdd16f0d58eea7c
Michael <64390529+mic-isa@users.noreply.github.com>
2023-12-07 16:40:25 +0100

    first

---
+++ app.js
@@ -0,0 +1,69 @@
+const dealine = document.querySelector(".container");
+
+const time = document.querySelectorAll(".time p");
+
+let tempDate = new Date();
+
+let tempYear = tempDate.getFullYear();
+
+let tempmonth = tempDate.getMonth();
+
+let tempDay = tempDate.getDate(); 
+
+let futureDate = new Date(tempYear, tempmonth, tempDay + 10, 11, 30, 0);
+
+let futureTime = futureDate.getTime();
+
+function countDown() {
+
+  let today = new Date().getTime();
+
+  let t = futureTime - today;
+
+  let oneDay = 24 * 60 * 60 * 1000;
+
+  let oneHour = 60 * 60 * 1000;
+
+  let oneMinute = 60 * 1000;
+
+  let oneSecond = 1000;
+
+  let days = Math.floor(t / oneDay);
+
+  let hours = Math.floor((t % oneDay) / oneHour);
+
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+
+  let seconds = Math.floor((t % oneMinute) / oneSecond);
+
+  let values = [days, hours, minutes, seconds];
+
+  function format(item) {
+
+    if (item < 10) {
+
+      return `0${item}`;
+
+    }
+    return item.toString(); 
+    
+  }
+
+  time.forEach(function (item, index) {
+
+    item.innerHTML = format(values[index]);
+    /*item.classList.add("top-flip")*/
+
+  });
+
+  if (t < 0) {
+
+    dealine.innerHTML = `<p class="expire"> Sorry, this has expired </p>`; 
+
+  }
+}
+
+setInterval(countDown, 1000);
+countDown();
+
+
